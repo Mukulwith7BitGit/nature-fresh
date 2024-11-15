7454b0256092d38aa134be800074515cdad8d41c
Mukul Dalal <mukuldalalnfs@gmail.com>
2024-11-15 23:54:06 +0530

    chore: Add mongoDb setup

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
 
 const app: express.Application = express();
@@ -13,6 +13,16 @@ dotenv.config({ path: './.env' });
 let hostName: string | undefined = process.env.HOST_NAME;
 let port: number | undefined = Number(process.env.PORT)
 let mongoDbUrl: string | undefined = process.env.MONGODB_URL;
+
+// mongoDb connection
+if (mongoDbUrl) {
+    mongoose.connect(mongoDbUrl).then((response: Mongoose) => {
+        console.log("Connected to MongoDB successfully...");
+    }).catch((err) => {
+        console.error(err);
+        process.exit(1);
+    })
+}
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).json({
