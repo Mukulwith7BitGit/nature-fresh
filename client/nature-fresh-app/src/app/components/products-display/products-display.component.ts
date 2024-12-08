f81557f4ba26787237b286301c85d161faafac7a
Mukul Dalal <mukuldalalnfs@gmail.com>
2024-12-09 00:01:40 +0530

    feat: add display, newly created products feature

--- client/nature-fresh-app/src/app/components/products-display/products-display.component.ts
+++ client/nature-fresh-app/src/app/components/products-display/products-display.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { ProductView } from '../../models/ProductView';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-display',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products-display.component.html',
   styleUrls: ['./products-display.component.css'],
 })
-export class ProductsDisplayComponent {
+export class ProductsDisplayComponent implements OnInit {
+
+  public products: ProductView[] = [] as ProductView[];
+  public errorMessage: string | undefined;
+
+  constructor(private productService: ProductService) { }
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe((data: ProductView[]) => {
+      this.products = data;
+    }, (error) => {
+      this.errorMessage = error;
+    });
+  }
 
 }
